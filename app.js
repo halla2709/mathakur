0a0f0d4ced0454f94d399b5a20ce044020ef4603
Halla <halla2709@gmail.com>
2017-09-12 19:59:17 +0000

    Database helpers ready

--- app.js
+++ app.js
@@ -2,52 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const database = require('./services/databaseHelper').db;
+const database = require('./services/databaseCreator').db;
+const dbHelper = require('./services/databaseHelper');
 
 const indexRouter = require('./routes/index.js');
 
-database.none("CREATE TABLE IF NOT EXISTS skoli(id SERIAL PRIMARY KEY, nafn varchar(40) NOT NULL)")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
-database.none("CREATE TABLE IF NOT EXISTS matur(id SERIAL PRIMARY KEY, nafn varchar(40) NOT NULL, flokkur varchar(40))")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
-database.none("CREATE TABLE IF NOT EXISTS notandi(id SERIAL PRIMARY KEY, nafn varchar(40) NOT NULL, galunafn varchar(20), inneign integer NOT NULL)")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
-database.none("CREATE TABLE IF NOT EXISTS stjornendur(id SERIAL PRIMARY KEY, notandanafn varchar(40) NOT NULL, password varchar(40) NOT NULL)")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
-database.none("CREATE TABLE IF NOT EXISTS skolimatur(skolaID integer, matarID integer, verd integer not null)")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
-database.none("CREATE TABLE IF NOT EXISTS uppahaldsmatur(notandaID integer, matarID1 integer, matarID2 integer, matarID3 integer, matarID4 integer, matarID5 integer)")
-.then(data => {
-    console.log('DATA:', data); // print data;
-})
-.catch(error => {
-    console.log('ERROR:', error); // print the error;
-});
+dbHelper.insertIntoTable(database, 'employee', ['name', 'nickname', 'credit'], ['halla', 'halla', 500])
+    .then(function() {
+        console.log("inserted into employee");
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+
+dbHelper.getFromTable(database, 'employee', ['name = \'halla\''])
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(error) {
+        console.error(error)
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- services/databaseHelper.js
+++ services/databaseHelper.js
@@ -1,18 +1,73 @@
-const pgp = require('pg-promise')({
-    query: e => {
-        console.log('QUERY:', e.query);
-    }
-});
+function getFromTable(db, tableName, conditions) {
+    let queryString = 'SELECT * FROM ' + replaceTableName(tableName);
+    if (conditions.length > 0) queryString += ' WHERE';
+    conditions.forEach(function (condition) {
+        queryString += " " + condition;
+    });
+    queryString += ";";
+    console.log(queryString);
+    return db.any(queryString, tableName);
+}
 
-const cn = {
-    host: 'localhost', // 'localhost' is the default;
-    port: 5432, // 5432 is the default;
-    database: 'mathakur',
-    user: 'mathakur',
-    password: 'mathakur'
-};
-const db = pgp(cn);
+function insertIntoTable(db, tableName, columns, values) {
+    let queryString = 'INSERT INTO ' + replaceTableName(tableName);
+    let columnsString = '(';
+    let valuesString = '(';
+    if (columns.length != values.length) throw "Column and value number mismatch";
+    for(let i = 0; i < columns.length-1; i++) {
+        columnsString += columns[i] + ', ';
+        valuesString += '$' + (i+1) + ', ';
+    }
+    columnsString += ' ' + columns[columns.length-1] + ')';
+    valuesString += ' $' + values.length + ')';
+    queryString += columnsString + " VALUES" + valuesString + ';';
+    console.log(queryString);
+    return db.none(queryString, values);
+}
+
+function updateCreditOfEmployee(db, employeeId, newCredit) {
+    let queryString = 'UPDATE employee SET credit = $1 WHERE id = $2';
+    return db.none(queryString, [newCredit, employeeId]);
+}
+
+function deleteFromTable(db, tableName, conditions) {
+    let queryString = 'DELETE FROM ' + replaceTableName(tableName);
+    if (conditions.length > 0) queryString += ' WHERE';
+    conditions.forEach(function (condition) {
+        queryString += " " + condition;
+    });
+    queryString += ";";
+    console.log(queryString);
+    return db.any(queryString, tableName);
+}
+
+function replaceTableName(tableName) {
+    switch (tableName) {
+        case "administator":
+            return 'administrator';
+            break;
+        case "employee":
+            return 'employee';
+            break;
+        case "favoritefood":
+            return 'favoritefood';
+            break;
+        case "food":
+            return 'food';
+            break;
+        case "foodprice":
+            return 'foodprice';
+            break;
+        case "school":
+            return 'school';
+            break;    
+        default:
+            console.error("no such table " + tableName);
+            break;
+    }
+}
 
 module.exports = {
-    db
-};
+    getFromTable,
+    insertIntoTable
+}
