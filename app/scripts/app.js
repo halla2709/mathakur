3193f0fc38745961a02f0e366df6fb3695741f7b
Halla <halla2709@gmail.com>
2017-09-12 23:30:16 +0000

    routing virkar jeij

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,36 +9,33 @@
  * Main module of the application.
  */
 angular
-  .module('mathakur', [])
-  .config(function($stateProvider, $urlRouterProvider) {
+  .module('mathakur', ['ui.router'])
+  .config(    
+    function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
-    $urlRouterProvider.otherwise('/index');
+    $urlRouterProvider.otherwise('/login');
 
     $stateProvider
-
-      .state('index', {
-        url: '/index',
-        parent: 'base',
-        templateUrl: 'index.html',
-        controller: 'LoginCtrl'  
-      
-        })
-        .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardCtrl'
-        })
-          .state('overview', {
-            url: '/overview',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
-          })
-          .state('reports', {
-            url: '/reports',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/reports.html'
-          });
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .state('overview', {
+        url: '/overview',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/overview.html'
+      })
+      .state('reports', {
+        url: '/reports',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/reports.html'
+      });
 
   });
