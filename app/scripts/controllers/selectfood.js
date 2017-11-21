62e6b5fe7d2f890e4806176b45251a2eda06fa77
Unnur <unnkbry@gmail.com>
2017-11-21 21:31:46 +0000

    allskonar skemmtilegt

--- app/scripts/controllers/selectfood.js
+++ app/scripts/controllers/selectfood.js
@@ -10,12 +10,15 @@
 angular.module('mathakur')
   .controller('SelectFoodCtrl', ['$scope', '$state', '$http', '$stateParams', function ($scope, $state, $http, $stateParams) {
 
+
     $scope.$state = $state;
     $scope.employee = $stateParams.param;
     $scope.receipt = [];
     $scope.currentSchool = 1;
     var path = '/food/' + $scope.currentSchool;
     $scope.total = 0;
+    $scope.myText = "text";
+
 
     $scope.addFood = function (food) {
 
@@ -32,33 +35,65 @@ angular.module('mathakur')
           break;
         }
       }
-
       if (index == -1) {
         food.quantity = 1;
         food.ordertotal = food.price;
         $scope.receipt.push(food);
-        
-
       }
-   
     };
 
     $scope.removeFood = function (food) {
       $scope.total -= food.price;
       for (var i = 0; i < $scope.receipt.length; i++) {
-
-       
         if ($scope.receipt[i].name === food.name) {
           $scope.receipt[i].quantity--;
           if ($scope.receipt[i].quantity == 0) {
             $scope.receipt.splice(i, 1);
           }
-          
           break;
         }
       }
     }
 
+    $scope.removeAllFood = function (food) {
+      $scope.total = 0;
+      for (var i = 0; i < $scope.receipt.length; i++) {
+        $scope.receipt.splice(i);
+      }
+    }
+
+    $scope.buyFood = function (food) {
+      console.log("employee á", $scope.employee.credit);
+      var credit = $scope.employee.credit;
+
+      if (confirm('Ertu viss um að þú viljir kaupa allt í körfunni?')) {
+        
+        if(credit >= $scope.total)
+        {
+              credit -= $scope.total;
+              $scope.employee.credit = credit;
+
+              console.log(credit);
+
+              $http({
+                method: 'PATCH',
+                url: 'employee/updatecredit/'+$scope.employee.id,
+                data: JSON.stringify({
+                    newCredit: credit
+                })
+          })
+          console.log(credit);
+          $scope.myText = "Þér hefur tekist að kaupa allt í körfunni! Eigðu góðan dag.";
+        }
+  
+        else {
+          console.log("Þú átt ekki nægann pening fyrir matnum!")
+          $scope.myText = "Þú átt ekki nægann pening fyrir matnum! Fjarlægðu úr körfu og reyndu aftur eða leggðu meira inn hjá þer"
+        }
+    } else {
+        // Do nothing!
+    }     
+    }
 
     $http.get("employee").then(function (response) {
         $scope.myDataEmployee = response.data;
