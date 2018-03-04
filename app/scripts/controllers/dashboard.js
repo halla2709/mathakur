86323d6d5b4d9ac2020f1f06a3643fa86dedd3b8
Halla <halla2709@gmail.com>
2018-03-04 20:26:25 +0000

    removed logs

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -115,13 +115,11 @@ angular.module('mathakur')
     }
 
     $http.get(employeePath).then(function (response) {
-      console.log(response.data);      
       response.data.sort(function(a, b){
         if(a.name < b.name) return -1;
         if(a.name > b.name) return 1;
         return 0;
       })
-      console.log(response.data);
         $scope.myDataEmployee = response.data;
       })
       .catch(function (response) {
