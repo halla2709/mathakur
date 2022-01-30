69a63493ea293f9752a8f08b56f371a75d0598de
Unnur <unnkbry@gmail.com>
2022-01-30 15:42:09 +0000

    cloudinary changes

--- server/services/cloudinaryHelper.js
+++ server/services/cloudinaryHelper.js
@@ -1,9 +1,11 @@
 const cloudinary = require("cloudinary");
+
 cloudinary.config({
-    cloud_name: 'dk7mpsfkw',
-    api_key: '431766444682953',
-    api_secret: 'YOJiDrJckmm7by_FDJVqWQiXcEk'
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME || 'rubyhallaunnur',
+    api_key: process.env.CLOUDINARY_APIKEY || '623827191818171',
+    api_secret: process.env.CLOUDINARY_APISECRET || 'xLuRo-m0BFw-9-ZS0rT0NeEn-CI'
 });
+
 const employeeDefaultUrl = 'tzeqj4l6kjyq0jptankn';
 const foodPhotoDefaultUrl = 'bazcykvn86tp963v8ocn';
 const employeePreset = 'j8gkhubq';
@@ -17,10 +19,10 @@ function savePhotoToCloudinary(req, res, next) {
         currentPreset = employeePreset;
     }
     if (typeof req.body.photo !== 'undefined' && req.body.photo !== '') {
-        cloudinary.v2.uploader.upload(req.body.photo, {upload_preset: currentPreset}, function (error, result) {
-            res.photoUrl = result.public_id;
-            next();
-        });
+        //cloudinary.v2.uploader.upload(req.body.photo, {upload_preset: currentPreset}, function (error, result) {
+        //    res.photoUrl = result.public_id;
+        //    next();
+        // });
     }
     else {
         res.photoUrl = currentDefaultUrl;
