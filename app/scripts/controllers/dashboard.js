13ec533631303107121df035da8a3debf47edcd4
Unnur <unnkbry@gmail.com>
2022-07-06 20:25:31 +0000

    laga 404 villu á mynd

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -160,3 +160,15 @@ angular.module('mathakur')
         });
     });
   }]);
+
+  angular.module('mathakur').directive('errSrc', function() {
+    return {
+      link: function(scope, element, attrs) {
+        element.bind('error', function() {
+          if (attrs.src != attrs.errSrc) {
+            attrs.$set('src', attrs.errSrc);
+          }
+        });
+      }
+    }
+  });
