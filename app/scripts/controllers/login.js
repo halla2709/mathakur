ad05d08c478a98e5ef031dd32b2c06cd48bddfd6
Halla <halla2709@gmail.com>
2022-10-22 14:41:26 +0000

    change login dropdown to a search bar

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -13,33 +13,55 @@ angular.module('mathakur')
     $scope.company = '';
     $scope.password = '';
     $scope.wrongpassword = false;
+    $scope.filteredCompanies = [];
+
+    $scope.onNewSearch = function() {
+      if ($scope.searchValue.length > 1) {
+        $scope.filteredCompanies = $scope.allCompanies.filter(function(comp) {
+          return comp.name.toLowerCase().startsWith($scope.searchValue.toLowerCase());
+        })
+      }
+      else {
+        $scope.filteredCompanies = [];
+      }
+    }
+
+    $scope.selectCompany = function(company) {
+      $scope.company = company;
+      $scope.filteredCompanies = [];
+      $scope.searchValue = $scope.company.name;
+    }
 
     $scope.submit = function () {
-      $scope.company = JSON.parse($scope.company);
-      var passwordHash = md5.createHash($scope.password || '');
-      var companyName = $scope.company.name;
-      server.post('/login/requestCompanyConnection', {
-          passwordHash: passwordHash
-        })
-        .then(function (responseJson) {
-          server.post('/login/loginCompany', {
-              companyName: companyName,
-              companyPassHash: md5.createHash(passwordHash + responseJson.data.companyRandomString)
-            })
-            .then(function (responseJson2) {
-              if (responseJson2.data.loggedIn) {
-                $rootScope.session.setCompany(responseJson2.data.loggedIn, 0);
-                $state.go('dashboard');
-              }
-              else {
-                $scope.company = '';
-                $scope.password = '';
-                $scope.wrongpassword = true;
-              }
-            })
-            .catch(function (error) { console.warn(error) })
-        })
-        .catch(function (error) { console.warn(error) });
+      if ($scope.filteredCompanies.length > 0) {
+        $scope.selectCompany($scope.filteredCompanies[0]);
+      }
+      else {
+        var passwordHash = md5.createHash($scope.password || '');
+        var companyName = $scope.company.name;
+        server.post('/login/requestCompanyConnection', {
+            passwordHash: passwordHash
+          })
+          .then(function (responseJson) {
+            server.post('/login/loginCompany', {
+                companyName: companyName,
+                companyPassHash: md5.createHash(passwordHash + responseJson.data.companyRandomString)
+              })
+              .then(function (responseJson2) {
+                if (responseJson2.data.loggedIn) {
+                  $rootScope.session.setCompany(responseJson2.data.loggedIn, 0);
+                  $state.go('dashboard');
+                }
+                else {
+                  $scope.company = '';
+                  $scope.password = '';
+                  $scope.wrongpassword = true;
+                }
+              })
+              .catch(function (error) { console.warn(error) })
+          })
+          .catch(function (error) { console.warn(error) });
+      }
 
       return false;
     }
@@ -49,7 +71,7 @@ angular.module('mathakur')
       {
         server.get("company")
           .then(function (response) {
-            $scope.myData = response.data;
+            $scope.allCompanies = response.data;
           })
           .catch(function (response) {
             //Error handle
